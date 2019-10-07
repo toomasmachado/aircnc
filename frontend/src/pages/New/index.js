44e0229bfbd1aacbee59f013c6a7d9e4d7c782b8
Tomas Turbina <toomasmac@gmail.com>
2019-10-07 00:50:01 -0300

    front

---
+++ frontend/src/pages/New/index.js
@@ -0,0 +1,62 @@
+import React, { useState, useMemo } from "react";
+import camera from "../../assets/camera.svg";
+import "./styles.css";
+
+export default function New() {
+  const { thumbnail, setThumbnail } = useState(null);
+  const { company, setCompany } = useState("");
+  const { techs, setTechs } = useState("");
+  const { price, setPrice } = useState("");
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  function handleSubmit() {}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        className={thumbnail ? "has-thumbnail" : ""}
+        style={{ backgroundImage: `url(${preview})` }}
+      >
+        <input
+          type="file"
+          onChange={event => setThumbnail(event.target.files[0])}
+        />
+        <img src={camera} alt="Select img" />
+      </label>
+      <label htmlFor="company">EMPRESA</label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrivel"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">
+        TECNOLOGIAS * <span>(separadas por vígula)</span>
+      </label>
+      <input
+        id="techs"
+        placeholder="Quais tecnologias usam"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">
+        VALOR DA DIÁRIA * <span>(em branco para gratuito)</span>
+      </label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">
+        Cadastrar
+      </button>
+    </form>
+  );
+}
